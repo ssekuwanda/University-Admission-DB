b91e322ff007479c26514ee7e40ae1780d16533f
ssekuwanda <cytixdoug@gmail.com>
2019-02-11 21:42:58 +0300

    adding whitenoise

--- univ/settings.py
+++ univ/settings.py
@@ -140,3 +140,5 @@ STATICFILES_DIRS = [
 ]
 # location where django collect all static files
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
