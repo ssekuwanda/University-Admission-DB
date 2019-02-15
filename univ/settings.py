5dd92bbcfe8c70fc9d66818835b91211ce36f3c4
ssekuwanda <cytixdoug@gmail.com>
2019-02-15 14:40:23 +0300

    debug removed

--- univ/settings.py
+++ univ/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$rosla1r%1d*i1(zua8&gb$-j#)%sj)*tw0&zp#x1fu09_hv%-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['universityadmin.herokuapp.com']
 
